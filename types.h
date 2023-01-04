7c18564d4f3b3614c1a954b3cb9ec744dd9aefb2
Ibai Burgos Sánchez <64355006+IbaiBuR@users.noreply.github.com>
2023-01-04 02:36:33 +0100

    change name

--- types.h
+++ types.h
@@ -4,7 +4,7 @@
  #define MAX_MOVES 1024
  #define N 50
  void getstats(FILE *, FILE *); //gets some stadistics of the input pgn file
- void deleteRtag(FILE *, FILE *); //deletes the result tag of the input pgn file and writes the output to another file
+ void deleteTags(FILE *, FILE *); //deletes the result tag of the input pgn file and writes the output to another file
  void getavgGD(FILE *, FILE *); //gets the average game duration of the input pgn file
  void getavgPC(FILE *, FILE *); //gets the average PlyCount of the input pgn file
  void getavgD(FILE *, FILE *); //gets the average depth per move of the input pgn file
