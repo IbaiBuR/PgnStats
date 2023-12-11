414d0f8156d29523e6e7c4a698f5a0ed14de7ed7
IbaiBuR <ibaibur@chessnibble.com>
2023-12-11 14:01:05 +0100

    Update cross-compilation script

--- cross_compile.sh
+++ cross_compile.sh
@@ -1,4 +1,4 @@
 cd src
-x86_64-w64-mingw32-gcc -o PgnStats main.c statistics.c util.c individual.c -lncursesw -DNCURSES_STATIC
+x86_64-w64-mingw32-gcc -I/usr/x86_64-w64-mingw32/sys-root/mingw/include/ -o PgnStats main.c statistics.c util.c individual.c -static -lncursesw -L/usr/x86_64-w64-mingw32/sys-root/mingw/bin/ -DNCURSES_STATIC
 mv PgnStats.exe ../binaries
 make clean
